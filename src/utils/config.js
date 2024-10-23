cf70f94a439702c6e0efdb8bff2e4592029d68db
Alausa <alausaabdulazeez@gmail.com>
2024-10-23 19:20:25 +0100

    changes baseurl to aws ipv4

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 // ENDPOINT URL
-export const BASE_URL = "https://emotion-diamond-be.onrender.com";
+export const BASE_URL = "http://51.20.87.34/";
+// export const BASE_URL = "https://emotion-diamond-be.onrender.com";
 // export const BASE_URL = "http://localhost:5000";
 
 // DEFAULT SUCCESS STATUS VARIABLE
