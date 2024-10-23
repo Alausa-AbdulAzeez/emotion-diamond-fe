d77aec110c63b36af35ba879ffe690465c6b6fa7
Alausa <alausaabdulazeez@gmail.com>
2024-10-23 10:25:21 +0100

    changed base url to production URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 // ENDPOINT URL
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://emotion-diamond-be.onrender.com";
 
 // DEFAULT SUCCESS STATUS VARIABLE
 export const API_SUCCESS_VARIABLE = "success";
