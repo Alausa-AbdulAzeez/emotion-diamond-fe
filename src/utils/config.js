8841202a370d05e117837be8e8757fbf5d887b7d
Alausa <alausaabdulazeez@gmail.com>
2024-10-24 00:48:31 +0100

    used a custom domain name instead

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,8 @@
 // ENDPOINT URL
-export const BASE_URL = "http://51.20.87.34/";
-// export const BASE_URL = "https://emotion-diamond-be.onrender.com";
-// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://analyser.com.ng";
+// export const BASE_URL = "http://51.20.87.34/"; AWS Public IP address
+// export const BASE_URL = "https://emotion-diamond-be.onrender.com"; //Render URL
+// export const BASE_URL = "http://localhost:5000"; // Local URL
 
 // DEFAULT SUCCESS STATUS VARIABLE
 export const API_SUCCESS_VARIABLE = "success";
