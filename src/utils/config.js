03f177ac73f89e26dbc0a32b1cb8684d86c313a3
Alausa <alausaabdulazeez@gmail.com>
2024-10-23 12:13:31 +0100

    reverted back to production backend

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // ENDPOINT URL
-// export const BASE_URL = "https://emotion-diamond-be.onrender.com";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://emotion-diamond-be.onrender.com";
+// export const BASE_URL = "http://localhost:5000";
 
 // DEFAULT SUCCESS STATUS VARIABLE
 export const API_SUCCESS_VARIABLE = "success";
